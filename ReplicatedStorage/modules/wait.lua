021c4f85dc1094f4c2f660eabd915c3cf474bc55
0aoq <73413330+0aoq@users.noreply.github.com>
2021-06-06 16:52:11 -0400

    Update wait.lua

--- ReplicatedStorage/modules/wait.lua
+++ ReplicatedStorage/modules/wait.lua
@@ -11,17 +11,22 @@ print("1 second has passed.")
 
 ]]
 
+--[[
+@function Custom wait module
+@param {number} x: the time to wait
+]]
+
 return function(x)
-	x = x or 0.0001 -- default x to 0.0001 if not chosen
-	
-	local begun = tick() -- get when the wait started
-	
-	while true do
-		RunService.Stepped:Wait() -- wait for runservice to step (on frame)
-		
-		local passed = tick() - begun -- get the time that has passed
-		if passed >= x then
-			return passed, begun -- if the time passed if more or equal to the wait time return
-		end
-	end
+    x = x or 0.0001 -- default x to 0.0001 if not chosen
+    
+    local begun = tick() -- get when the wait started
+    
+    while true do
+        RunService.Stepped:Wait() -- wait for runservice to step (on frame)
+        
+        local passed = tick() - begun -- get the time that has passed
+        if passed >= x then
+            return passed, begun -- if the time passed if more or equal to the wait time return
+        end
+    end
 end
